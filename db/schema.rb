c7806c07c29a5a8c9de323db5878aef8d0333c2b
Raul Muniz <Raul.Muniz01@utrgv.edu>
2017-12-05 23:52:59 -0600

    Dashboard
    dash -template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121172749) do
+ActiveRecord::Schema.define(version: 20171206054717) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
