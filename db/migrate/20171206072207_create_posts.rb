453f7a4664c436afb51e8ef27fcae7dd579d1da1
Raul Muniz <Raul.Muniz01@utrgv.edu>
2017-12-06 01:24:03 -0600

    text fix
    better representation

--- db/migrate/20171206072207_create_posts.rb
+++ db/migrate/20171206072207_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :content
-      t.string :author
+      t.string :availability
+      t.text :comment
+      t.string :time
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206054717) do
+ActiveRecord::Schema.define(version: 20171206072207) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "author"
+    t.string "availability"
+    t.text "comment"
+    t.string "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
