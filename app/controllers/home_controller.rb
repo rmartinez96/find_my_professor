946e69781b14c93c8b5e9e7716fc061ee790aa43
Raul Martinez Jr <raul.martinez01@utrgv.edu>
2017-12-08 23:01:17 -0600

    Fixed the nav bar
    Fixed the nav bar to make it look pretty.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
   	if user_signed_in?
   		#redirect_to rails_admin.dashboard_path
+
   	end
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get '/', to: "home#index"
   get 'home/index'
 
-  devise_for :users
+  devise_for :users 
   get "/admin/index", to: "admin#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
