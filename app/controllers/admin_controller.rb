d62173929681ed4abfdbd2e473afff961ba805ba
Unknown <rigo.sandoval.r.s97@gmail.com>
2017-12-10 00:21:09 -0600

    Added role ids/redirects for professors & students
    models >user>role_ids
    controllers>admin_controllers==def/redirect

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,8 @@
 class AdminController < ApplicationController
 	before_action :authenticate_user!
 	before_action :admin_only!
+	before_action :professor_only!
+	before_action :student_only!
 
 	def index
 	end
@@ -11,6 +13,18 @@ class AdminController < ApplicationController
 	def admin_only!
 		if !current_user.admin?
 			redirect_to "/"
+		end
+	end
+
+	def professor_only!
+		if !current_user.professor?
+			redirect_to "/dashboardprofs"
+		end
+	end
+
+	def student_only!
+		if !current_user.student?
+			redirect_to "/dashboardstuds"
 		end
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ApplicationRecord
   	return role_id == 99
   end
 
+
+  def professor?
+  	return role_id == 80
+  end
+
+  def student?
+  	return role_id == 70
+  end
+
 end
 
 
