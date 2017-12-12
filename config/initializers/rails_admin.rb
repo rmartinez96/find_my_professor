57612d936b3c534f050a6426dc5e3f249a91661f
Raul Muniz <Raul.Muniz01@utrgv.edu>
2017-12-11 18:57:33 -0600

    Further advances on Dashboard
    Include email of user on dashboard and sign out option to direct to homepage.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,10 +1,5 @@
 RailsAdmin.config do |config|
-
-
-config.authorize_with do
-    redirect_to main_app.root_path unless warden.user.admin == true
-  end
-
+  config.current_user_method(&:current_user)
   ### Popular gems integration
 
   ## == Devise ==
