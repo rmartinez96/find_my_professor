83e67eb4473686aff3461a78d9def8888003b33e
Raul Martinez Jr <raul.martinez01@utrgv.edu>
2017-12-13 00:18:28 -0600

    Attempt to fix Heroku
    The problem is
    NameError: uninitialized constant RailsAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/admin', as: :rails_admin
   get '/', to: "home#index"
   get 'home/index'
 
